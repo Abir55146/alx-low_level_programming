dfb26042fe7897ac5f5caffc0a6a4990bffc8c7d
Abir <abiromer7@gmail.com>
2023-09-10 10:44:39 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,16 @@
-#include <stdio.h
+#include <stdio.h> 
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints with put 
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multiling        ual puzzle");
+	return(0);
+	
+}
