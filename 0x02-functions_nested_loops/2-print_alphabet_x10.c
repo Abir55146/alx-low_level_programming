3814f687c0cbf95d57b93609a20c24c7184993e7
Abir <abiromer7@gmail.com>
2023-09-12 23:08:05 +0200

    10xalpabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 function that will   *          print the alphabet 10 times
+ * print_alphabet_x10  will print the alph *  abet 10 times
  *
 */
 
@@ -10,9 +10,9 @@ void print_alphabet_x10(void);
 	int line. ch;
 	for (line = 0; line <= 9; line++)
 	{
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
+          for (ch = 'a'; ch <= 'z'; ch++)
+	        	_putchar(ch);
+        	_putchar('\n');
 	}
 }	
 	
