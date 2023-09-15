cb550d9a355927c42028f18fbd4ed8c71f1d316c
Abir <abiromer7@gmail.com>
2023-09-15 21:36:19 +0200

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints the alphabet
+ *         in lowercase and upper case
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)7
+{
+
+        char letter;
+
+
+        for (letter = 'a'; letter <= 'z';                        letter++)
+	{
+
+        if (letter != 'e' && letter != 'q')
+
+        putchar(letter);
+        }
+        putchar('\n');
+        return (0);
+
+}
