1fc77b9bb21aaffcf10ff28274d930ac245eccf9
Abir <abiromer7@gmail.com>
2023-09-28 21:40:00 +0200

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,14 +12,11 @@ void _puts_recursion(char *s)
 {
 
         if (*s)
-        {
-
-                _putchar(*s);
-
+	{
+		_putchar(*s);
                 _puts_recursion(s + 1);
 
         }
         else
                 _putchar('\n');
-
 }
