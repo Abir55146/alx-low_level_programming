b578a09d70aea07c189f2d48a186bcd9041ec08c
Abir <abiromer7@gmail.com>
2023-10-03 23:51:23 +0200

    Float like a butterfly, sting like a bee

--- 0-create_array.c/0-create_array.c
+++ 0-create_array.c/0-create_array.c
@@ -5,25 +5,27 @@
 /**
  * create_array - create array of size siz
  *    e and assign char c
- *
  * @size: size of array
  * @c: char to assign
+ *
  * Description: create array of size size
  *   and assign char c
  *
  * Return: pointer to array, NULL if fail
 */
 
-char *create_array(unsigned int size, char     c)
+char *create_array(unsigned int size, charc)
 {
 
-        char *str;
-        unsigned int i;
+	char *str;
+        
+	unsigned int i;
 
-
-        str = malloc(sizeof(char) * size);
-        if (size == 0 || str == NULL)
-                return (NULL);
+	str = malloc(sizeof(char) * size);
+        
+	if (size == 0 || str == NULL)
+                
+		return (NULL);
 
 
         for (i = 0; i < size; i++)
