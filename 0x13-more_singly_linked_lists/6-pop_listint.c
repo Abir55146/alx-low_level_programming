1643d933295838e20054df31f20c6de847d05d0e
Abir <abiromer7@gmail.com>
2023-12-11 14:32:48 +0200

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -17,14 +17,12 @@ int pop_listint(listint_t **head)
                      int num;
 
             if (!head || !*head)
-
-               return (0);
+		    return (0);
 
                num = (*head)->n;
                temp = (*head)->next;
                         free(*head);
                        *head = temp;
-   
 		       return (num);
 
 }
