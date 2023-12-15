92693386e1f51607aa5dc3ba977c3e1b46903291
Abir <abiromer7@gmail.com>
2023-12-15 14:01:32 +0200

    1. 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -19,7 +19,7 @@ unsigned long int _pow(unsigned int base, unsigned int power)
         num = 1;
         for (a = 1; a <= power; a++)
                 num *= base;
-        return (num);
+	return (num);
 
 }
 
@@ -37,32 +37,24 @@ void print_binary(unsigned long int n)
 
         unsigned long int divisor, check;
         char flag;
-
-        flag = 0;
-
-        divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
 
         while (divisor != 0)
         {
                 check = n & divisor;
                 if (check == divisor)
                 {
-	
-          
-			 
-			  flag = 1;
-                        _putchar('1');
-
-                }
-                else if (flag == 1 || divisor == 1)
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
                 {
-
-                        _putchar('0');
+			_putchar('0');
 
                 }
-                divisor >>= 1;
-
-        }
+		divisor >>= 1;
+	}
 
 }
 
